203ccce3b643840b0e1c4531195235525e0135bf
enzonsn <enzonsperduti@gmail.com>
2021-07-28 22:31:08 -0500

    started models

--- models/index.js
+++ models/index.js
@@ -4,17 +4,12 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, { foreignKey: 'cat_id' });
 
-// Categories have many Products
+Category.hasMany(Product, { foreignKey: 'cat_id' });
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+module.exports = { Product, Category, Tag, ProductTag };
